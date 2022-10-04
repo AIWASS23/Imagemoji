ca08ed5e5e2f6bce52b288e69d2471d6f8a5a603
AIWASS23 <marcelodearaujo677@gmail.com>
2022-10-04 08:56:43 -0300

    Initial Commit

---
+++ Imagemoji/ImagemojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagemojiApp.swift
+//  Imagemoji
+//
+//  Created by Marcelo De Araújo on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ImagemojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
